c976104e101bbe35ac9c542b63d1a741ef17647a
Nathan Collier <nathaniel.collier@gmail.com>
2023-09-18 16:07:23 -0400

    restructure into package

---
+++ intake_esgf/__init__.py
@@ -0,0 +1,5 @@
+# An intake and intake-esm inpsired catalog for ESGF
+
+from intake_esgf.catalog import ESGFCatalog
+
+__all__ = ["ESGFCatalog"]
--- intake_esgf/catalog.py
+++ intake_esgf/catalog.py
@@ -1,19 +1,20 @@
-import hashlib
 import logging
-import re
-import time
 import warnings
 from pathlib import Path
-from typing import Any, Dict, List, Union
+from typing import Dict, Union
 
 import pandas as pd
-import requests
 import xarray as xr
 from globus_sdk import SearchClient
-from globus_sdk.response import GlobusHTTPResponse
 from tqdm import tqdm
 
-# CESM2 has used multiple fill values and throws lots of ugly warnings
+from intake_esgf.core import (
+    get_dataset_pattern,
+    response_to_dataframe,
+    response_to_https_download,
+    response_to_local_filelist,
+)
+
 warnings.simplefilter("ignore", category=xr.SerializationWarning)
 
 # create an alias for the different indices we want to support. Could easily extend to
@@ -22,10 +23,11 @@ GLOBUS_INDEX_IDS = {
     "anl-dev": "d927e2d9-ccdb-48e4-b05d-adbc3d97bbc5",
 }
 
-# setup logging
+# setup logging, not sure if this belongs here but if the catalog gets used I want logs
+# dumped to this location.
 local_cache = Path.home() / ".esgf"
 local_cache.mkdir(parents=True, exist_ok=True)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("intake-esgf")
 log_file = local_cache / "esgf.log"
 if not log_file.is_file():
     log_file.touch()
@@ -38,192 +40,6 @@ file_handler.setFormatter(
 )
 logger.addHandler(file_handler)
 logger.setLevel(logging.INFO)
-
-
-def get_file_hash(filepath: Union[str, Path], algorithm: str) -> str:
-    """Get the file has using the given algorithm."""
-    algorithm = algorithm.lower()
-    assert algorithm in hashlib.algorithms_available
-    sha = hashlib.__dict__[algorithm]()
-    with open(filepath, "rb") as fp:
-        while True:
-            data = fp.read(64 * 1024)
-            if not data:
-                break
-            sha.update(data)
-    return sha.hexdigest()
-
-
-def download_and_verify(
-    url: str,
-    local_file: Union[str, Path],
-    hash: str,
-    hash_algorithm: str,
-    content_length: Union[None, int] = None,
-) -> None:
-    """Download the url to a local file and check for validity, removing if not."""
-    if not isinstance(local_file, Path):
-        local_file = Path(local_file)
-    local_file.parent.mkdir(parents=True, exist_ok=True)
-    resp = requests.get(url, stream=True)
-    resp.raise_for_status()
-    if content_length is None and "content-length" in resp.headers:
-        content_length = int(resp.headers.get("content-length"))
-    transfer_time = time.process_time()
-    with open(local_file, "wb") as fdl:
-        with tqdm(
-            total=content_length,
-            unit="B",
-            unit_scale=True,
-            desc=local_file.name,
-            ascii=True,
-        ) as pbar:
-            for chunk in resp.iter_content(chunk_size=1024):
-                if chunk:
-                    fdl.write(chunk)
-                    pbar.update(len(chunk))
-    transfer_time = time.process_time() - transfer_time
-    rate = content_length * 1e-6 / transfer_time
-    if get_file_hash(local_file, hash_algorithm) != hash:
-        logger.info(f"{local_file} failed validation, removing")
-        local_file.unlink()
-        return
-    logger.info(
-        f"Downloaded {url} to {local_file} in {transfer_time:.2f} [s] at {rate:.2f} [Mb s-1] with {hash_algorithm}={hash}"  # noqa: E501
-    )
-
-
-def get_relative_esgf_path(entry: Dict[str, Any]) -> Path:
-    """Return the relative ESGF path from the Globus entry."""
-    if "content" not in entry:
-        raise ValueError("'content' not part of the entry.")
-    content = entry["content"]
-    if set(["version", "dataset_id", "directory_format_template_"]).difference(
-        content.keys()
-    ):
-        raise ValueError("Entry content does not contain expected keys.")
-    # For some reason, the `version` in the globus response is just an integer and not
-    # what is used in the file path so I have to parse it out of the `dataset_id`
-    content["version"] = [content["dataset_id"].split("|")[0].split(".")[-1]]
-    # Format the file path using the template in the response
-    file_path = content["directory_format_template_"][0]
-    file_path = Path(
-        file_path.replace("%(root)s/", "")
-        .replace("%(", "{")
-        .replace(")s", "[0]}")
-        .format(**content)
-    )
-    return file_path
-
-
-def response_to_dataframe(response: GlobusHTTPResponse, pattern: str) -> pd.DataFrame:
-    """Return a pandas dataframe from the response of a Globus search."""
-    df = []
-    for g in response["gmeta"]:
-        assert len(g["entries"]) == 1  # A check on the assumption of a single entry
-        # Manually remove entries which are not datasets. With the way I am using
-        # search, this is not necesary but if a file type gets through the regular
-        # expression pattern will break and pollute the dataframe.
-        if g["entries"][0]["entry_id"] != "dataset":
-            continue
-        m = re.search(pattern, g["subject"])
-        if m:
-            df.append(m.groupdict())
-            # Also include the globus 'subject' so we can find files later when we are
-            # ready to download.
-            df[-1]["globus_subject"] = g["subject"]
-    df = pd.DataFrame(df)
-    return df
-
-
-def response_to_local_filelist(
-    response: GlobusHTTPResponse, data_root: Union[str, Path]
-) -> List[Path]:
-    """Return a list of local paths to netCDF files described in the Globus response.
-
-    This function is used to check if the files for which we searched are already
-    available locally. This could be because we have previously downloaded them and they
-    are in the local cache. It could also be because we are on a resource that has
-    direct access to the ESGF data and we have called `set_esgf_data_root()`. This
-    function uses the `directory_format_template_` and values in the response to form a
-    relative location of the files represented in the Globus response. We then prepend
-    the given `data_root` to make the path absolute and check for existence.
-
-    """
-    assert data_root is not None
-    if isinstance(data_root, str):
-        data_root = Path(data_root)
-    if not data_root.is_dir():
-        raise FileNotFoundError(f"Directory {data_root} does not exist.")
-    paths = []
-    for g in response["gmeta"]:
-        assert len(g["entries"]) == 1
-        entry = g["entries"][0]
-        if entry["entry_id"] != "file":
-            continue
-        file_path = data_root / get_relative_esgf_path(entry)
-        if not file_path.is_dir():
-            raise FileNotFoundError(f"Directory {file_path} does not exist.")
-        for file_name in file_path.glob("*.nc"):
-            paths.append(file_name)
-        if paths:
-            logger.info(f"Using files from {file_path}")
-    return paths
-
-
-def response_to_https_download(
-    response: GlobusHTTPResponse, local_root: Union[str, Path]
-) -> List[Path]:
-    """Download the file using the links found in the globus response."""
-    if isinstance(local_root, str):
-        local_root = Path(local_root)
-    if not local_root.is_dir():
-        raise FileNotFoundError(f"Directory {local_root} does not exist.")
-    paths = []
-    for g in response["gmeta"]:
-        assert len(g["entries"]) == 1
-        entry = g["entries"][0]
-        if entry["entry_id"] != "file":
-            continue
-        content = entry["content"]
-        url = [u for u in content["url"] if u.endswith("|HTTPServer")]
-        if len(url) != 1:
-            continue
-        url = url[0].replace("|HTTPServer", "")
-        local_file = local_cache / get_relative_esgf_path(entry) / Path(Path(url).name)
-        local_file.parent.mkdir(parents=True, exist_ok=True)
-        try:
-            download_and_verify(
-                url,
-                local_file,
-                content["checksum"][0],
-                content["checksum_type"][0],
-                int(content["size"]),
-            )
-            paths.append(local_file)
-        except requests.exceptions.HTTPError:
-            logger.info(f"HTTP error {url}")
-    return paths
-
-
-def get_dataset_pattern() -> str:
-    """Return the Globus subject regular expression pattern for datasets."""
-    COLUMNS = [
-        "mip_era",
-        "activity_id",
-        "institution_id",
-        "source_id",
-        "experiment_id",
-        "member_id",
-        "table_id",
-        "variable_id",
-        "grid_label",
-        "version",
-        "data_node",
-    ]
-    pattern = "\.".join([f"(?P<{c}>\S[^.|]+)" for c in COLUMNS[:-1]])
-    pattern += f"\|(?P<{COLUMNS[-1]}>\S+)"
-    return pattern
 
 
 class ESGFCatalog:
@@ -427,25 +243,3 @@ class ESGFCatalog:
             else:
                 ds[key] = "Could not obtain this file."
         return ds
-
-
-if __name__ == "__main__":
-    # The ESGF catalog initializes to nothing and needs populated with an initial
-    # search. This uses the globus sdk to query their index and the response is parsed
-    # into a pandas dataframe for viewing.
-    cat = ESGFCatalog().search(
-        strict=True,
-        activity_id="CMIP",
-        experiment_id="historical",
-        source_id="CESM2",
-        variable_id=["gpp", "areacella", "sftlf"],
-        member_id=["r1i1p1f1"],
-    )
-
-    # Printing the catalog will list the unique values in each column of the dataframe,
-    # can also call unique() to get this information.
-    print(cat)
-
-    # Our version of `to_dataset_dict()` should try to do the best thing for the user
-    # automatically.
-    ds = cat.to_dataset_dict()
