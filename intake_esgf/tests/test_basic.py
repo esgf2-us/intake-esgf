a9eca2b1636ed3fdd29c3046e19576642f774849
Nathan Collier <nathaniel.collier@gmail.com>
2024-01-25 14:14:22 -0500

    improved coverage that runs faster

--- intake_esgf/tests/test_basic.py
+++ intake_esgf/tests/test_basic.py
@@ -1,26 +1,41 @@
 from intake_esgf import ESGFCatalog
+from intake_esgf.exceptions import NoSearchResults
 
 
 def test_search():
-    cat = ESGFCatalog().search(
-        activity_id="CMIP",
+    cat = ESGFCatalog(esgf1_indices="esgf-node.llnl.gov")
+    print(cat)
+    cat = ESGFCatalog(esgf1_indices=["esgf-node.llnl.gov"]).search(
         experiment_id="historical",
-        source_id="CESM2",
-        variable_id=["gpp", "areacella", "sftlf"],
+        source_id="CanESM5",
+        variable_id=["gpp"],
         variant_label=["r1i1p1f1"],
     )
-    assert len(cat.df) == 3
+    print(cat)
+    ds = cat.to_dataset_dict()
+    assert "gpp" in ds
+    assert "sftlf" in ds["gpp"]
 
 
-def test_global_search():
-    cat = ESGFCatalog(esgf1_indices=True).search(
-        activity_id="CMIP",
+def test_esgroot():
+    cat = ESGFCatalog()
+    cat.set_esgf_data_root(cat.local_cache)
+    cat.search(
         experiment_id="historical",
-        source_id="CESM2",
-        variable_id=["gpp", "areacella", "sftlf"],
+        source_id="CanESM5",
+        variable_id=["gpp"],
         variant_label=["r1i1p1f1"],
     )
-    assert len(cat.df) == 3
+    ds = cat.to_dataset_dict(add_measures=False)
+    assert "gpp" in ds
+
+
+def test_noresults():
+    cat = ESGFCatalog()
+    try:
+        cat.search(variable_id="does_not_exist")
+    except NoSearchResults:
+        pass
 
 
 def test_tracking_ids():
@@ -32,24 +47,35 @@ def test_tracking_ids():
         [
             "hdl:21.14100/0577d84f-9954-494f-8cc8-465aa4fd910e",
             "hdl:21.14100/0972f78b-158e-4c6b-bcdf-7d0d75d7a8cd",
-            "hdl:21.14100/0e4dfb8f-b677-456e-abc7-71e1ebc16deb",
-            "hdl:21.14100/17b6c62f-455b-49bc-8674-564f7ca5ed6a",
-            "hdl:21.14100/1bd030c9-1761-4fca-911e-6ea2b6407bc7",
-            "hdl:21.14100/2844ea5a-4589-4ed4-bbb7-c13e9964a4b7",
         ]
     )
-    assert len(cat.df) == 7
+    assert len(cat.df) == 2
 
 
 def test_add_cell_measures():
     # these measures are in r1i1p1f2 / piControl
     cat = ESGFCatalog().search(
-        variable_id="gpp",
+        variable_id="mrros",
         source_id="UKESM1-0-LL",
         variant_label="r2i1p1f2",
         frequency="mon",
         experiment_id="historical",
     )
-    ds = cat.to_dataset_dict()["gpp"]
+    ds = cat.to_dataset_dict()["mrros"]
     assert "sftlf" in ds
     assert "areacella" in ds
+
+
+def test_log():
+    cat = ESGFCatalog().search(
+        experiment_id="historical",
+        source_id="CanESM5",
+        variable_id=["gpp"],
+        variant_label=["r1i1p1f1"],
+    )
+    assert "CanESM5" in cat.session_log()
+
+
+def test_download_dbase():
+    cat = ESGFCatalog()
+    assert len(cat.download_summary().columns)
