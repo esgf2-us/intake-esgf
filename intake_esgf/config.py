f1ebf038a8ce39a0638999fb9304c82004cea4a2
Nathan Collier <nathaniel.collier@gmail.com>
2024-04-10 09:22:29 -0400

    add a config object

---
+++ intake_esgf/config.py
@@ -0,0 +1,152 @@
+"""Configuration for intake-esgf"""
+
+import contextlib
+import copy
+from pathlib import Path
+
+import yaml
+
+defaults = {
+    "globus_indices": {
+        "anl-dev": True,
+        "ornl-dev": True,
+    },
+    "solr_indices": {
+        "esgf.ceda.ac.uk": False,
+        "esgf-data.dkrz.de": False,
+        "esgf-node.ipsl.upmc.fr": False,
+        "esg-dn1.nsc.liu.se": False,
+        "esgf-node.llnl.gov": False,
+        "esgf.nci.org.au": False,
+        "esgf-node.ornl.gov": False,
+    },
+    "esg_dataroot": [
+        "/p/css03/esgf_publish",
+        "/eagle/projects/ESGF2/esg_dataroot",
+        "/global/cfs/projectdirs/m3522/cmip6/",
+    ],
+    "local_cache": [
+        "~/.esgf/",
+    ],
+    "logfile": "~/.config/intake-esgf/esgf.log",
+    "download_db": "~/.config/intake-esgf/download.db",
+}
+
+
+class Config(dict):
+    """A global configuration object used in the package."""
+
+    def __init__(self, filename: Path | None = None, **kwargs):
+        self.filename = (
+            Path(filename)
+            if filename is not None
+            else Path.home() / ".config/intake-esgf/conf.yaml"
+        )
+        self.reload_all()
+        self.temp = None
+        super().__init__(**kwargs)
+
+    def __repr__(self):
+        return yaml.dump(dict(self))
+
+    def reset(self):
+        """Return to defaults."""
+        self.clear()
+        self.update(defaults)
+
+    def save(self, filename: Path | None = None):
+        """Save current configuration to file as YAML."""
+        filename = filename or self.filename
+        filename.parent.mkdir(parents=True, exist_ok=True)
+        with open(filename, "w") as f:
+            yaml.dump(dict(self), f)
+
+    @contextlib.contextmanager
+    def _unset(self, temp):
+        yield
+        self.clear()
+        self.update(temp)
+
+    def set(
+        self,
+        *,
+        indices: dict[str, bool] = {},
+        all_indices: bool = False,
+        esg_dataroot: list[str] | None = None,
+        local_cache: list[str] | None = None
+    ):
+        """Change intake-esgf configuration options.
+
+        Parameters
+        ----------
+        indices
+            A dictionary of indices whose use status you wish to change.
+        all_indices
+            Enable to check all indices for dataset information.
+        esg_dataroot
+            A list of read-only locations that we will check for ESGF data.
+        local_cache
+            A list of locations where we read and write data to, prefering the first
+            entry.
+
+        Examples
+        --------
+        >>> intake-esgf.conf.set(indices={'esgf-node.ornl.gov': True})
+
+        """
+        temp = copy.deepcopy(self)
+        self["globus_indices"].update(
+            {
+                key: value
+                for key, value in indices.items()
+                if key in self["globus_indices"]
+            }
+        )
+        self["solr_indices"].update(
+            {
+                key: value
+                for key, value in indices.items()
+                if key in self["solr_indices"]
+            }
+        )
+        if all_indices:
+            for key in self["globus_indices"]:
+                self["globus_indices"][key] = True
+            for key in self["solr_indices"]:
+                self["solr_indices"][key] = True
+        if esg_dataroot is not None:
+            self["esg_dataroot"] = esg_dataroot
+        if local_cache is not None:
+            self["local_cache"] = local_cache
+        return self._unset(temp)
+
+    def __getitem__(self, item):
+        if item in self:
+            return super().__getitem__(item)
+        elif item in defaults:
+            return defaults[item]
+        else:
+            raise KeyError(item)
+
+    def get(self, key, default=None):
+        if key in self:
+            return super().__getitem__(key)
+        return default
+
+    def reload_all(self):
+        self.reset()
+        self.load()
+
+    def load(self, filename: Path | None = None):
+        """Update global config from YAML file or default file if None."""
+        filename = filename or self.filename
+        if filename.is_file():
+            with open(filename) as f:
+                try:
+                    self.update(yaml.safe_load(f))
+                except Exception:
+                    pass
+
+
+conf = Config()
+conf.reload_all()
