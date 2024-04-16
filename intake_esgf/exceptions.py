95df57d7b269c5571e69325e526e99aa5afecc64
Nathan Collier <nathaniel.collier@gmail.com>
2024-04-16 11:50:43 -0400

    exception for localcache

--- intake_esgf/exceptions.py
+++ intake_esgf/exceptions.py
@@ -1,5 +1,7 @@
 """Exceptions for intake-esgf."""
 
+from pathlib import Path
+
 
 class IntakeESGFException(Exception):
     """Exceptions from the intake-esgf package."""
@@ -11,3 +13,13 @@ class NoSearchResults(IntakeESGFException):
 
 class SearchError(IntakeESGFException):
     """Search was used incorrectly."""
+
+
+class LocalCacheNotWritable(IntakeESGFException):
+    """You do not have permission to write in the cache directories."""
+
+    def __init__(self, paths: list[Path]):
+        self.paths = paths
+
+    def __str__(self):
+        return f"You do not have write permission in the cache directories specified: {self.paths}"
