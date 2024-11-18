6ad99528951c6d7eb8c965dc521ef6d1bbfe2ef8
Nathan Collier <nathaniel.collier@gmail.com>
2024-11-18 12:08:27 -0500

    added tests

--- intake_esgf/tests/test_projects.py
+++ intake_esgf/tests/test_projects.py
@@ -1,4 +1,6 @@
+import intake_esgf
 from intake_esgf import ESGFCatalog
+from intake_esgf.exceptions import ProjectHasNoFacet
 
 
 def test_cmip5():
@@ -21,3 +23,16 @@ def test_cmip3():
     )
     cat.remove_ensembles()
     assert len(cat.model_groups()) == 24
+
+
+def test_obs4mips():
+    # this project is only on LLNL for now
+    with intake_esgf.conf.set(indices={"esgf-node.llnl.gov": True}):
+        cat = ESGFCatalog().search(
+            project="obs4MIPs", institution_id="NASA-LaRC", variable_id="rlus"
+        )
+        try:  # this shouldn't work but fail nicely
+            cat.model_groups()
+        except ProjectHasNoFacet:
+            pass
+        assert len(cat.df) == 1
