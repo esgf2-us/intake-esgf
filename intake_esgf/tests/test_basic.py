899834b134a6f6bdc5b46860af658ac51d94ced1
Bouwe Andela <b.andela@esciencecenter.nl>
2025-07-09 17:02:58 +0200

    Add a test that searches are cached

--- intake_esgf/tests/test_basic.py
+++ intake_esgf/tests/test_basic.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from time import perf_counter
 
 import pytest
 from requests_cache import CachedSession
@@ -27,12 +28,42 @@ def test_search():
         assert "sftlf" in ds["gpp"]
 
 
-def test_indices_are_cached():
+def test_indices_use_cached_session_by_default():
     with intake_esgf.conf.set(all_indices=True):
         cat = ESGFCatalog()
         assert len(cat.indices) > 1
         for ind in cat.indices:
             assert isinstance(ind.session, CachedSession)
+
+
+@pytest.mark.parametrize("index_type", ["globus", "stac", "solr"])
+def test_search_is_cached(index_type, tmp_path):
+    # Use a test specific cache.
+    intake_esgf.conf["requests_cache"]["cache_name"] = str(
+        tmp_path / "requests-cache.sqlite"
+    )
+    # Enable only the index type being tested.
+    for index_collection in ["globus_indices", "solr_indices", "stac_indices"]:
+        for key in intake_esgf.conf[index_collection]:
+            intake_esgf.conf[index_collection][key] = (
+                index_collection == f"{index_type}_indices"
+            )
+
+    facets = {
+        "experiment_id": ["historical"],
+        "source_id": ["CanESM5"],
+        "variable_id": ["tas"],
+        "variant_label": ["r1i1p1f1"],
+    }
+
+    # Test that a repeat search is faster than the initial one.
+    start = perf_counter()
+    ESGFCatalog().search(**facets)
+    initial_search = perf_counter() - start
+    start = perf_counter()
+    ESGFCatalog().search(**facets)
+    cached_search = perf_counter() - start
+    assert cached_search < initial_search
 
 
 def test_esgroot():
