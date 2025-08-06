a698b8eb07abe302737ff4cc2edf5e1a1762d5fe
Nathan Collier <nathaniel.collier@gmail.com>
2025-08-06 08:42:06 -0400

    use a bigger file so it doesn't download before the safety time

--- intake_esgf/tests/test_download.py
+++ intake_esgf/tests/test_download.py
@@ -11,7 +11,7 @@ def test_slow_cancel():
     intake_esgf.conf.set(slow_download_threshold=100)  # unreasonably fast speed
     with pytest.raises(StalledDownload):
         download_and_verify(
-            url="https://esgf-node.ornl.gov/thredds/fileServer/css03_data/CMIP6/ScenarioMIP/MIROC/MIROC-ES2L/ssp245/r5i1p1f2/Amon/prc/gn/v20201222/prc_Amon_MIROC-ES2L_ssp245_r5i1p1f2_gn_201501-210012.nc",
+            url="https://esgf-node.ornl.gov/thredds/fileServer/css03_data/CMIP6/ScenarioMIP/MIROC/MIROC-ES2L/ssp245/r5i1p1f2/Amon/hus/gn/v20201222/hus_Amon_MIROC-ES2L_ssp245_r5i1p1f2_gn_201501-210012.nc",
             local_file=Path("tmp.nc"),
             hash=None,
             hash_algorithm=None,
