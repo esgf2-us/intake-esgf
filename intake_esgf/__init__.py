d3aff118b3fc1956de6730d0e57a301e1a6e7fb6
Nathan Collier <nathaniel.collier@gmail.com>
2026-05-21 10:10:42 -0400

    remove functions we no longer need

--- intake_esgf/__init__.py
+++ intake_esgf/__init__.py
@@ -7,21 +7,6 @@ import xarray as xr
 warnings.simplefilter("ignore", category=xr.SerializationWarning)
 
 
-def in_notebook() -> bool:
-    """Check if the code is running in a jupyter notebook"""
-    try:
-        # Function is defined if calling from a notebook
-        shell = get_ipython().__class__.__name__  # type: ignore
-        if shell == "ZMQInteractiveShell":  # Jupyter notebook, Spyder or qtconsole
-            return True
-        elif shell == "TerminalInteractiveShell":
-            return False  # Terminal running IPython
-        else:
-            return False  # Other type (?)
-    except NameError:
-        return False  # Probably standard Python interpreter
-
-
 def supported_projects() -> list[str]:
     """What projects are supported?"""
     from intake_esgf.projects import projects
@@ -29,10 +14,8 @@ def supported_projects() -> list[str]:
     return list(projects.keys())
 
 
-IN_NOTEBOOK: bool = in_notebook()
-
 from intake_esgf.catalog import ESGFCatalog  # noqa
 from intake_esgf.config import conf  # noqa
 from intake_esgf._version import __version__  # noqa
 
-__all__ = ["ESGFCatalog", "conf", "IN_NOTEBOOK", "supported_projects"]
+__all__ = ["ESGFCatalog", "conf", "supported_projects"]
