93d681fe3b9428abe4874018e5339238faf2e403
Nathan Collier <nathaniel.collier@gmail.com>
2026-09-04 17:02:38 -0400

    update stac tests with consistent data

--- intake_esgf/tests/core/test_stac.py
+++ intake_esgf/tests/core/test_stac.py
@@ -3,61 +3,67 @@ import pickle
 import pytest
 
 from intake_esgf.core import STACESGFIndex
-from intake_esgf.exceptions import NoSearchResults
-
-# INDEX = STACESGFIndex("discovery.integration.esgf-west.org")
 
 
 @pytest.fixture
-def ceda():
-    return STACESGFIndex("api.stac.esgf.ceda.ac.uk")
+def east():
+    return STACESGFIndex("discovery.east.esgf.io")
 
 
 @pytest.fixture
 def west():
-    return STACESGFIndex("discovery.integration.esgf-west.org")
+    return STACESGFIndex("discovery.west.esgf.io")
 
 
 def test_search_cmip6_west(west):
     df = west.search(
         project="CMIP6",
-        experiment_id="amip",
-        source_id="E3SM-1-0",
+        source_id="UKESM1-0-LL",
         variable_id="tas",
-    )
-    assert len(df) == 1
-    infos = west.get_file_info(df["id"].to_list())
-    assert len(infos) == 6
-
-
-def test_search_cmip7_west(west):
-    df = west.search(
-        project="CMIP7",
+        frequency="mon",
         experiment_id="historical",
-        source_id="CanESM5-1",
-        variable_id="tas",
+        variant_label="r1i1p1f2",
     )
     assert len(df) == 1
     infos = west.get_file_info(df["id"].to_list())
     assert len(infos) == 2
 
 
-def test_search_cmip6plus_west(west):
+def test_search_cmip7_west(west):
     df = west.search(
-        project="CMIP6Plus",
-        experiment_id="hist-lu",
-        source_id="HadGEM3-GC31-LL",
-        variable_id="areacella",
+        project="CMIP7",
+        source_id="UKCM2a-0-HH",
+        variable_id="tas",
+        frequency="mon",
+        experiment_id="historical",
+        variant_label="r1i1p1f1",
     )
     assert len(df) == 1
     infos = west.get_file_info(df["id"].to_list())
-    assert len(infos) == 1
+    assert len(infos) == 18
+
+
+def test_search_cmip6plus_west(west):
+    df = west.search(
+        project="CMIP6Plus",
+        source_id="ACCESS-CM2",
+        variable_id="zg500",
+        experiment_id="tbi-pace-I-clim-mod",
+        table_id="AE6hrPt",
+        variant_label="r1i1p1f1",
+    )
+    assert len(df) == 1
+    infos = west.get_file_info(df["id"].to_list())
+    assert len(infos) == 20
 
 
 @pytest.mark.xfail(raises=ValueError)
-def test_search_cmip6_ceda(ceda):
-    # This test should only return 1 item but because their CMIP6 is incorrectly published, returns many
-    df = ceda.search(
+def test_search_cmip6_ceda(east):
+    # This test should only return 1 item but because their CMIP6 is incorrectly
+    # published, returns many. We search for a particular member_id here, but
+    # all s1960-s2018 are returned because that field is incorrect in the
+    # record.
+    df = east.search(
         project="CMIP6",
         experiment_id="dcppA-hindcast",
         member_id="s2020-r1i1p1f2",
@@ -66,25 +72,13 @@ def test_search_cmip6_ceda(ceda):
     )
     if len(df) != 1:
         raise ValueError(f"Data is erroneously published on CEDA, {len(df)=} != 1")
-    infos = ceda.get_file_info(df["id"].to_list())
+    infos = east.get_file_info(df["id"].to_list())
     if len(infos) != 12:
         raise ValueError(f"Data is erroneously published on CEDA, {len(infos)=} != 12")
 
 
-@pytest.mark.parametrize("project", ["CMIP6Plus", "CMIP7"])
-@pytest.mark.xfail(raises=NoSearchResults)
-def test_search_ceda_fails(ceda, project):
-    # Nothing is published in these so they should fail. Wh
-    df = ceda.search(
-        project=project,
-    )
-    assert len(df) == 0
-    infos = ceda.get_file_info(df["id"].to_list())
-    assert len(infos) == 0
-
-
-def test_pickle(ceda) -> None:
-    index = ceda
+def test_pickle(east) -> None:
+    index = east
     pickled = pickle.dumps(index)
     unpickled = pickle.loads(pickled)
     assert repr(index) == repr(unpickled)
